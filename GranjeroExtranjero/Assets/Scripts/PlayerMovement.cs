2668980a82a75655b42a0c44a5a0b9daf9ba15a4
DanielGarciaFlorida <dagana@alumnatflorida.es>
2025-10-27 01:36:21 +0100

    Correcion asset URL
    Correcion problema con universal render pipeline y error que habia borrado el script de player movement

---
+++ GranjeroExtranjero/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+	public float speed;       
+	public float gravity;      
+	public float impulse;      
+	public float groundY;     
+
+	private bool isGrounded = true;
+
+	void Start()
+	{
+		speed = 5.0f;
+		gravity = 9.8f;
+		impulse = 7.0f;
+		groundY = -4.5f;
+	}
+
+	void Update()
+	{
+		float moveHorizontal = Input.GetAxis("Horizontal");
+		transform.position += new Vector3(moveHorizontal * Time.deltaTime * 5f, 0, 0);
+		if (isGrounded && Input.GetKeyDown(KeyCode.Space))
+		{
+			speed = impulse;
+			isGrounded = false;
+		}
+
+
+		if (!isGrounded)
+		{
+			speed -= gravity * Time.deltaTime;
+			transform.position += Vector3.up * speed * Time.deltaTime;
+
+		}	
+			if (transform.position.y <= groundY)
+			{
+				transform.position = new Vector3(transform.position.x, groundY, transform.position.z);
+				isGrounded = true;
+				speed = 0f; new Vector2(transform.position.x, transform.position.y);
+		}
+
+	}
+}
