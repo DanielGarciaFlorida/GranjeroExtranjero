832ef2e3bf3944a8af6df359fd4f2c7a782a6ac5
Juhevi01 <127087606+JulioHerraez@users.noreply.github.com>
2025-11-21 09:58:47 +0100

    He mejorado el script del enemigo

---
+++ GranjeroExtranjero/Assets/Scripts/Enemy_Script.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+
+public class Enemy_Script : MonoBehaviour
+{
+    
+     private float speed = 3f;            
+     private float detectionRange = 5f;   
+     private float stopDistance = 1.2f;   
+
+    
+     public Transform player;            
+
+    private void Update()
+    {
+        if (player == null) return;
+
+        
+        float distance = Mathf.Abs(player.position.x - transform.position.x);
+
+        if (distance <= detectionRange && distance > stopDistance)
+        {
+            
+            float directionX = Mathf.Sign(player.position.x - transform.position.x);
+
+            
+            transform.position += new Vector3(directionX * speed * Time.deltaTime, 0f, 0f);
+
+            
+            Vector3 scale = transform.localScale;
+            scale.x = Mathf.Abs(scale.x) * directionX;
+            transform.localScale = scale;
+        }
+    }
+}
