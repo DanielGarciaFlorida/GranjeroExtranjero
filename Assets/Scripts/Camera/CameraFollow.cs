5f2c6083d6444e5d625561ef93e33e21478ec9c0
Juhevi01 <127087606+JulioHerraez@users.noreply.github.com>
2026-01-06 02:37:29 +0100

    Hice script de la camara para seguir al jugador fluidamente

---
+++ Assets/Scripts/Camera/CameraFollow.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    
+    public Transform target;
+
+    
+    public Vector2 offset;
+
+    
+    [Range(0.01f, 1f)]
+    public float smoothTime = 0.2f;
+
+    private Vector3 velocity = Vector3.zero;
+
+    void LateUpdate()
+    {
+        if (target == null) return;
+
+        Vector3 targetPosition = new Vector3(
+            target.position.x + offset.x,
+            target.position.y + offset.y,
+            transform.position.z
+        );
+
+        transform.position = Vector3.SmoothDamp(
+            transform.position,
+            targetPosition,
+            ref velocity,
+            smoothTime
+        );
+    }
+}
